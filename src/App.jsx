c39f9daa1f9504b92b3b328f2b4c0fcb1a8fcf99
unknown <rt125280@gmail.com>
2024-11-06 16:25:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Data from './Components/Data'
+import "./Components/Mediaqury.css"
+
+function App() {
+  return (
+    <>
+    <div className='weathers'>
+      <Data />
+
+    </div>
+    </>
+  )
+}
+
+export default App
